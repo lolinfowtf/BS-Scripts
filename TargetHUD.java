6bdac0985224b3d2a2c8c86750599e721a62823f
oslurpie <132771490+oslurpie@users.noreply.github.com>
2025-03-08 13:32:00 +0000

    New Update
    Removed useless options, Renamed some stuff and fixed issue's

--- TargetHUD.java
+++ TargetHUD.java
@@ -1,4 +1,4 @@
-double targetHudX, targetHudY;
+double PositionX, PositionY;
 float currentHealthBarFillWidth = 1.0f;
 HashMap<String, Image> playerHeads = new HashMap<>();
 float lastHealthBarFillWidth = 1.0f;
@@ -6,35 +6,38 @@ int ghostBarTicks = 0;
 int damageTicks = 0;
 float lastTargetHealth = -1;
 boolean followPlayer = false;
+String[] Colors = {"Cherry", "Cotton Candy", "Flare", "Flower", "Gold", "Grayscale", "Royal", "Sky", "Vine"};
 
 void onLoad() {
-    modules.registerSlider("TargetHud X", "px", 250, 0, 700, 1);
-    modules.registerSlider("TargetHud Y", "px", 220, 0, 500, 1);
-    modules.registerSlider("Damage Alpha", "%", 100, 0, 150, 1);
-    modules.registerSlider("Damage Effect Duration", "ticks", 37, 0, 100, 1);
+    modules.registerSlider("Position X", 250, 0, 700, 1);
+    modules.registerSlider("Position Y", 220, 0, 500, 1);
     modules.registerButton("Show Avatar", true);
     modules.registerButton("Follow Player", false);
-    modules.registerSlider("Background Red", "R", 30, 0, 255, 1);
-    modules.registerSlider("Background Green", "G", 30, 0, 255, 1);
-    modules.registerSlider("Background Blue", "B", 30, 0, 255, 1);
-    modules.registerSlider("Background Alpha", "A", 180, 0, 255, 1);
-    modules.registerSlider("Ghost Bar Duration", "ticks", 30, 0, 100, 1);
-    modules.registerSlider("Ghost Bar Alpha", "A", 100, 0, 255, 1);
-    modules.registerSlider("HealthBar Start Red", "R", 0, 0, 255, 1);
-    modules.registerSlider("HealthBar Start Green", "G", 255, 0, 255, 1);
-    modules.registerSlider("HealthBar Start Blue", "B", 0, 0, 255, 1);
-    modules.registerSlider("HealthBar End Red", "R", 255, 0, 255, 1);
-    modules.registerSlider("HealthBar End Green", "G", 0, 0, 255, 1);
-    modules.registerSlider("HealthBar End Blue", "B", 0, 0, 255, 1);
-
+    modules.registerSlider("Background Alpha", 180, 0, 255, 1);
+    modules.registerSlider("Ghost Bar Duration", " Ticks", 30, 0, 100, 1);
+    modules.registerSlider("Ghost Bar Alpha", 100, 0, 255, 1);
+    modules.registerSlider("Color Mode", "", 0, Colors);
 }
+
+Color[][] colorModes = {
+    { new Color(255, 200, 200), new Color(243, 58, 106) }, 
+    { new Color(99, 249, 255), new Color(255, 104, 204) }, 
+    { new Color(231, 39, 24), new Color(245, 173, 49) }, 
+    { new Color(215, 166, 231), new Color(211, 90, 232) }, 
+    { new Color(255, 215, 0), new Color(240, 159, 0) }, 
+    { new Color(240, 240, 240), new Color(110, 110, 110) }, 
+    { new Color(125, 204, 241), new Color(30, 71, 170) }, 
+    { new Color(160, 230, 225), new Color(15, 190, 220) }, 
+    { new Color(17, 192, 45), new Color(201, 234, 198) }
+};
+
 
 void onPreUpdate() {
     followPlayer = modules.getButton(scriptName, "Follow Player");
 
     if (!followPlayer) {
-        targetHudX = modules.getSlider(scriptName, "TargetHud X");
-        targetHudY = modules.getSlider(scriptName, "TargetHud Y");
+        PositionX = modules.getSlider(scriptName, "Position X");
+        PositionY = modules.getSlider(scriptName, "Position Y");
     }
 
     for (NetworkPlayer player : world.getNetworkPlayers()) {
@@ -45,7 +48,6 @@ void onPreUpdate() {
 }
 
 void onRenderTick(float partialTicks) {
- 
     Entity target = modules.getKillAuraTarget();
     if (target != null) {
         if (followPlayer) {
@@ -56,23 +58,13 @@ void onRenderTick(float partialTicks) {
         int nameWidth = render.getFontWidth(targetDisplayName);
         float hudWidth = Math.max(100, nameWidth + 30);
         float hudHeight = 32;
-        float x = (float) targetHudX;
-        float y = (float) targetHudY;
+        float x = (float) PositionX;
+        float y = (float) PositionY;
 
         boolean showAvatar = modules.getButton(scriptName, "Show Avatar");
 
-        int bgRed = (int) modules.getSlider(scriptName, "Background Red");
-        int bgGreen = (int) modules.getSlider(scriptName, "Background Green");
-        int bgBlue = (int) modules.getSlider(scriptName, "Background Blue");
         int bgAlpha = (int) modules.getSlider(scriptName, "Background Alpha");
-        int startRed = (int) modules.getSlider(scriptName, "HealthBar Start Red");
-        int startGreen = (int) modules.getSlider(scriptName, "HealthBar Start Green");
-        int startBlue = (int) modules.getSlider(scriptName, "HealthBar Start Blue");
-
-        int endRed = (int) modules.getSlider(scriptName, "HealthBar End Red");
-        int endGreen = (int) modules.getSlider(scriptName, "HealthBar End Green");
-        int endBlue = (int) modules.getSlider(scriptName, "HealthBar End Blue");
-        Color backgroundColor = new Color(bgRed, bgGreen, bgBlue, bgAlpha);
+        Color backgroundColor = new Color(30, 30, 30, bgAlpha); 
         float totalHudWidth = showAvatar ? hudWidth + 32 : hudWidth;
 
         render.rect(x, y, x + totalHudWidth - 15, y + hudHeight, backgroundColor.getRGB());
@@ -82,17 +74,16 @@ void onRenderTick(float partialTicks) {
         int damageEffectDuration = (int) modules.getSlider(scriptName, "Damage Effect Duration");
 
         float previousHealth = lastTargetHealth;
+        lastTargetHealth = targetHealth;
 
-lastTargetHealth = targetHealth;
+        if (previousHealth > targetHealth) {
+            damageTicks = damageEffectDuration;
+            ghostBarTicks = (int) modules.getSlider(scriptName, "Ghost Bar Duration");
+            lastHealthBarFillWidth = currentHealthBarFillWidth;
+        }
 
-if (previousHealth > targetHealth) { 
-    damageTicks = damageEffectDuration;
-    ghostBarTicks = (int) modules.getSlider(scriptName, "Ghost Bar Duration");
-    lastHealthBarFillWidth = currentHealthBarFillWidth;
-}
-if (ghostBarTicks > 0) ghostBarTicks--;
-if (damageTicks > 0) damageTicks--;
-
+        if (ghostBarTicks > 0) ghostBarTicks--;
+        if (damageTicks > 0) damageTicks--;
 
         float nameXPosition = showAvatar ? x + 26 : x + 5;
         if (showAvatar) {
@@ -102,7 +93,7 @@ if (damageTicks > 0) damageTicks--;
 
                 if (damageTicks > 0) {
                     int damageAlpha = (int) (modules.getSlider(scriptName, "Damage Alpha") * ((float) damageTicks / damageEffectDuration));
-                    render.rect(x + 3, y + 3, x + 23, y + 23, new Color(255, 0, 0, damageAlpha).getRGB());
+                    render.rect(x + 3, y + 3, x + 23, y + 23, new Color(255, 0, 0, 100).getRGB());
                 }
             }
         }
@@ -115,46 +106,40 @@ if (damageTicks > 0) damageTicks--;
 
         float healthBarY = y + hudHeight - 6;
         render.rect(x + 2, healthBarY, x + 2 + healthBarWidth, healthBarY + 3, new Color(50, 50, 50).getRGB());
+        int colorMode = (int) modules.getSlider(scriptName, "Color Mode");
+        Color startColor = colorModes[colorMode][0];
+        Color endColor = colorModes[colorMode][1];
 
 
-       
         if (ghostBarTicks > 0) {
-    int ghostAlpha = (int) modules.getSlider(scriptName, "Ghost Bar Alpha");
+            int ghostAlpha = (int) modules.getSlider(scriptName, "Ghost Bar Alpha");
 
-    Color ghostStartColor = new Color(startRed, startGreen, startBlue, ghostAlpha);
-    Color ghostEndColor = new Color(endRed, endGreen, endBlue, ghostAlpha);
+            Color ghostStartColor = new Color(startColor.getRed(), startColor.getGreen(), startColor.getBlue(), ghostAlpha);
+            Color ghostEndColor = new Color(endColor.getRed(), endColor.getGreen(), endColor.getBlue(), ghostAlpha);
 
-    render.gradientRect(
-        x + 2 + currentHealthBarFillWidth, healthBarY, 
-        x + 2 + lastHealthBarFillWidth, healthBarY + 3,
-        ghostStartColor.getRGB(),
-        ghostEndColor.getRGB()
-    );
-}
+            render.gradientRect(x + 2 + currentHealthBarFillWidth, healthBarY, 
+                x + 2 + lastHealthBarFillWidth, healthBarY + 3,
+                ghostStartColor.getRGB(), ghostEndColor.getRGB());
+        }
 
+   
+        render.gradientRect(x + 2, healthBarY, x + 2 + currentHealthBarFillWidth, healthBarY + 3,
+            startColor.getRGB(), endColor.getRGB());
 
+ 
+        Entity playerIg = client.getPlayer();
+        if (playerIg != null) {
+            float playerHealth = playerIg.getHealth();
+            float healthDifference = playerHealth - targetHealth;
+            String healthDiffText = String.format("%+.1f", healthDifference);
 
-       render.gradientRect(x + 2, healthBarY, x + 2 + currentHealthBarFillWidth, healthBarY + 3,
-    new Color(startRed, startGreen, startBlue).getRGB(),
-    new Color(endRed, endGreen, endBlue).getRGB()
-);
+            float healthDiffX = x + 5 + healthBarWidth - render.getFontWidth(healthDiffText) - 2;
+            float healthDiffY = healthBarY - 8;
 
-  Entity playerIg = client.getPlayer();
-if (playerIg != null) {
-    float playerHealth = playerIg.getHealth();
-    float healthDifference = playerHealth - targetHealth;
-    String healthDiffText = String.format("%+.1f", healthDifference);
-    float healthDiffX = x + 2 + currentHealthBarFillWidth - render.getFontWidth(healthDiffText);
-
-    float healthDiffY = healthBarY - 8;
-    
-  
-    render.text2d(healthDiffText, healthDiffX, healthDiffY, 0.9f, new Color(255, 255, 255).getRGB(), true);
-}
+            render.text2d(healthDiffText, healthDiffX, healthDiffY, 0.9f, new Color(255, 255, 255).getRGB(), true);
+        }
 
 
-
-  
         float itemX = nameXPosition;
         float itemY = y + 11;
         float itemScale = 0.8f;
@@ -176,6 +161,8 @@ if (playerIg != null) {
     }
 }
 
+
+
 double interpolate(double current, double old, float scale) {
     return old + (current - old) * scale;
 }
@@ -195,7 +182,7 @@ void updateFollowPlayerPosition(Entity entity, float partialTicks) {
     Vec3 screenPos = render.worldToScreen(interpolatedX, interpolatedY + entity.getHeight(), interpolatedZ, client.getDisplaySize()[2], partialTicks);
 
     if (screenPos != null) {
-        targetHudX = screenPos.x - 50;
-        targetHudY = screenPos.y - 25;
+        PositionX = screenPos.x - 50;
+        PositionY = screenPos.y - 25;
     }
 }
